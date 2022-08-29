6bc8c5129b146ab0f07ff7ec262dab2eaf9f39d1
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2022-08-29 11:37:52 -0700

    .tar file name changed; increasing # of concurrent compiler when building ISCE3

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPO=opera-adt
+REPO=opera
 IMAGE=rtc
 TAG=interface_0.1
 
@@ -19,7 +19,7 @@ docker build --rm --force-rm --network host -t $REPO/$IMAGE:$TAG -f Docker/Docke
 #docker run --rm -u "$(id -u):$(id -g)" -v "$PWD:/mnt" -w /mnt -it --network host "${IMAGE}:$t" pytest /mnt/tests/
 
 # create image tar
-docker save $REPO/$IMAGE:$TAG > Docker/dockerimg_rtc_interface.tar
+docker save $REPO/$IMAGE:$TAG > Docker/dockerimg_rtc_interface_0.1.tar
 
 # remove image
 docker image rm $REPO/$IMAGE:$TAG
