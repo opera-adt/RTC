5839066f498cb259eae8cc7ce884aa198da2a49f
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2022-09-29 21:29:12 -0700

    revision on printout message

--- app/rtc_compare.py
+++ app/rtc_compare.py
@@ -67,7 +67,7 @@ def print_data_difference(val_1, val_2, indent=4):
         if np.any(mask_nan_discrepancy):
             num_pixel_nan_discrepancy = mask_nan_discrepancy.sum()
             index_pixel_nan_discrepancy = np.where(mask_nan_discrepancy)
-            print(f'{str_indent} Found {num_pixel_nan_discrepancy} '
+            print(f'{str_indent} Found {num_pixel_nan_discrepancy} of '
                    'NaN inconsistecy between the input arrays. '
                   f'First index of the discrepancy: [{index_pixel_nan_discrepancy[0][0]}]')
 
