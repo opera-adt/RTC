7d18f6334608854aeae6968d2d19b65741b394e8
Seongsu Jeong <sjeong.kr@gmail.com>
2022-10-07 13:33:48 -0700

    Update app/rtc_compare.py
    Co-authored-by: Gustavo H. X. Shiroma <52007211+gshiroma@users.noreply.github.com>

--- app/rtc_compare.py
+++ app/rtc_compare.py
@@ -135,7 +135,7 @@ def get_list_dataset_attrs_keys(hdf_obj_1: h5py.Group,
 
     '''
     Recursively traverse the datasets and attributes within the input HDF5 group.
-    Returns the list of keys for the dataset and the attributes.
+    Returns the list of keys for datasets and attributes.
 
     NOTE:
     In case of attributes, the path and the attribute keys are
