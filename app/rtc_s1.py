6f2e8afab9d2a06a296dc9f3208ed76d4d92d95a
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-02-20 10:31:32 -0800

    checkpoint before mod on path

--- app/rtc_s1.py
+++ app/rtc_s1.py
@@ -1068,7 +1068,7 @@ def run(cfg: RunConfig):
     for filename in temp_files_list:
         if not os.path.isfile(filename):
             continue
-        os.remove(filename)
+        # os.remove(filename) # TODO: Temporary suppression. remove before commit
 
         logger.info(f'    {filename}')
 
