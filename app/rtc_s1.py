290a8dd2ba105100721ee6d7b9f28b513fbb749e
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-03-03 11:29:25 -0800

    docstring revised; child processing result check added

--- app/rtc_s1.py
+++ app/rtc_s1.py
@@ -1096,6 +1096,9 @@ def run(cfg: RunConfig):
     t_end = time.time()
     logger.info(f'elapsed time: {t_end - t_start}')
 
+    # Return value to indicate that the processing has completed succesfully
+    return 0
+
 
 def get_radar_grid(geogrid, dem_interp_method_enum, product_prefix,
                    output_dir, extension, save_incidence_angle,
