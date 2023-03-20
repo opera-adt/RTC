dfa4dc084a7451da4065bd0e3cce1f2361c7e236
Gustavo Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2023-03-20 15:36:10 -0700

    make workflow test more general

--- tests/test_rtc_s1_workflow.py
+++ tests/test_rtc_s1_workflow.py
@@ -4,15 +4,147 @@ import os
 import requests
 import glob
 import tarfile
+from osgeo import gdal
 
 from rtc.runconfig import RunConfig, load_parameters
 from rtc.core import create_logger
 from rtc.rtc_s1_single_job import get_rtc_s1_parser, run_single_job
+from rtc.rtc_s1 import run_parallel
 
 
 from rtc.version import VERSION as SOFTWARE_VERSION
 
 FLAG_ALWAYS_DOWNLOAD = False
+
+
+def _load_cfg_parameters(cfg):
+
+    load_parameters(cfg)
+
+    # Load parameters
+    product_version_float = cfg.groups.product_group.product_version
+    if product_version_float is None:
+        product_version = SOFTWARE_VERSION
+    else:
+        product_version = f'{product_version_float:.1f}'
+    output_dir = cfg.groups.product_group.output_dir
+    product_id = cfg.groups.product_group.product_id
+    if product_id is None:
+        product_id = 'rtc_product'
+    product_prefix = f'{product_id}_v{product_version}'
+
+    output_imagery_format = \
+        cfg.groups.product_group.output_imagery_format
+    save_imagery_as_hdf5 = (output_imagery_format == 'HDF5' or
+                            output_imagery_format == 'NETCDF')
+    save_secondary_layers_as_hdf5 = \
+        cfg.groups.product_group.save_secondary_layers_as_hdf5
+
+    save_metadata = (cfg.groups.product_group.save_metadata or
+                     save_imagery_as_hdf5 or
+                     save_secondary_layers_as_hdf5)
+
+    if output_imagery_format == 'NETCDF':
+        hdf5_file_extension = 'nc'
+    else:
+        hdf5_file_extension = 'h5'
+
+    if save_imagery_as_hdf5 or output_imagery_format == 'COG':
+        output_raster_format = 'GTiff'
+    else:
+        output_raster_format = output_imagery_format
+
+    if output_raster_format == 'GTiff':
+        imagery_extension = 'tif'
+    else:
+        imagery_extension = 'bin'
+
+    return output_dir, product_prefix, save_imagery_as_hdf5, \
+        save_secondary_layers_as_hdf5, save_metadata, \
+        hdf5_file_extension,imagery_extension
+
+
+def _is_valid_gdal_reference(gdal_reference):
+    try:
+        gdal_ds = gdal.Open(gdal_reference, gdal.GA_ReadOnly)
+        return gdal_ds is not None
+    except RuntimeError:
+        return False
+    return False
+
+
+def _check_results(output_dir, product_prefix, save_imagery_as_hdf5,
+        save_secondary_layers_as_hdf5, save_metadata, hdf5_file_extension,
+        imagery_extension):
+
+    # Check RTC-S1 HDF5 metadata
+    assert(save_metadata)
+    geo_h5_filename = os.path.join(
+        output_dir, f'{product_prefix}.{hdf5_file_extension}')
+
+    # Check RTC-S1 imagery
+    if save_imagery_as_hdf5:
+
+        # assert that VV image is present
+        geo_vv_file = (f'NETCDF:"{geo_h5_filename}":'
+                       '/science/SENTINEL1/RTC/grids/frequencyA/VV')
+        assert(_is_valid_gdal_reference(geo_vv_file))
+
+        # assert that HH image is not present
+        geo_hh_file = (f'NETCDF:"{geo_h5_filename}":'
+                       '/science/SENTINEL1/RTC/grids/frequencyA/HH')
+        assert(not(_is_valid_gdal_reference(geo_hh_file)))
+
+    else:
+    
+        # assert that VV image is present
+        geo_vv_filename = os.path.join(
+            output_dir, f'{product_prefix}_VV.{imagery_extension}')
+        assert(os.path.isfile(geo_vv_filename))
+
+        # assert that HH image is not present
+        geo_hh_filename = os.path.join(
+            output_dir, f'{product_prefix}_HH.{imagery_extension}')
+        assert(not(os.path.isfile(geo_hh_filename)))
+    
+    # Check RTC-S1 secondary layers
+    if save_secondary_layers_as_hdf5:
+
+        # assert that the following secondary layers are present:
+        ds_list = ['numberOfLooks', 'RTCAreaNormalizationFactor',
+                   'localIncidenceAngle']
+        for ds_name in ds_list:
+            current_file = (f'NETCDF:"{geo_h5_filename}":'
+                           '/science/SENTINEL1/RTC/grids/frequencyA/'
+                           f'{ds_name}')
+            assert(_is_valid_gdal_reference(current_file))
+
+        # assert that the following secondary layers are not present:
+        ds_list = ['incidenceAngle', 'projectionAngle']
+        for ds_name in ds_list:
+            current_file = (f'NETCDF:"{geo_h5_filename}":'
+                           '/science/SENTINEL1/RTC/grids/frequencyA/'
+                           f'{ds_name}')
+            assert(not(_is_valid_gdal_reference(current_file)))
+
+    else:
+        # assert that the following secondary layers are present:
+        ds_list = ['number_of_looks', 'rtc_area_normalization_factor',
+                   'local_incidence_angle']
+        for ds_name in ds_list:
+            current_file = os.path.join(
+                output_dir, f'{product_prefix}_'
+                f'{ds_name}.{imagery_extension}')
+            assert(os.path.isfile(current_file))
+
+        # assert that the following secondary layers are not present:
+        ds_list = ['incidence_angle', 'projectionAngle']
+        for ds_name in ds_list:
+            current_file = os.path.join(
+                output_dir, f'{product_prefix}_'
+                f'{ds_name}.{imagery_extension}')
+            assert(not(os.path.isfile(current_file)))
+
 
 def test_workflow():
 
@@ -52,56 +184,48 @@ def test_workflow():
 
     # Get a runconfig dict from command line argumens
     runconfig_path = os.path.join('data', 's1b_los_angeles', 'rtc_s1.yaml')
-    cfg = RunConfig.load_from_yaml(runconfig_path, 'rtc_s1')
 
-    load_parameters(cfg)
+    # for output_imagery_format in ['COG', 'HDF5']:
+    for output_imagery_format in ['COG']:
 
-    # Run geocode burst workflow
-    run_single_job(cfg)
-    product_version_float = cfg.groups.product_group.product_version
-    if product_version_float is None:
-        product_version = SOFTWARE_VERSION
-    else:
-        product_version = f'{product_version_float:.1f}'
-    output_dir = cfg.groups.product_group.output_dir
-    product_id = cfg.groups.product_group.product_id
-    if product_id is None:
-        product_id = 'rtc_product'
-    product_prefix = f'{product_id}_v{product_version}'
+        cfg = RunConfig.load_from_yaml(runconfig_path, 'rtc_s1')
+        cfg.groups.product_group.output_imagery_format = output_imagery_format
 
-    output_imagery_format = \
-        cfg.groups.product_group.output_imagery_format
-    save_imagery_as_hdf5 = (output_imagery_format == 'HDF5' or
-                            output_imagery_format == 'NETCDF')
-    save_secondary_layers_as_hdf5 = \
-        cfg.groups.product_group.save_secondary_layers_as_hdf5
+        output_dir_single_job, product_prefix, save_imagery_as_hdf5, \
+            save_secondary_layers_as_hdf5, save_metadata, \
+            hdf5_file_extension, imagery_extension = _load_cfg_parameters(cfg)
 
-    save_metadata = (cfg.groups.product_group.save_metadata or
-                     save_imagery_as_hdf5 or
-                     save_secondary_layers_as_hdf5)
+        # Testing creation of secondary layers:
+        #
+        # The YAML file above (`runconfig_path`) is only set to create the
+        # `numberOfLooks` and `RTCAreaNormalizationFactor`. Here,
+        # we also force the creation of `layoverShadowMask` and
+        # `localIncidenceAngle` and will test if they are present in the output files.
+        # We also assert that layers not set to be created,
+        # such as `incidenceAngle`, are indeed not created
 
-    if output_imagery_format == 'NETCDF':
-        hdf5_file_extension = 'nc'
-    else:
-        hdf5_file_extension = 'h5'
+        cfg.groups.processing.geocoding.save_layover_shadow_mask = True
+        cfg.groups.processing.geocoding.save_local_inc_angle = True
 
-    if save_imagery_as_hdf5 or output_imagery_format == 'COG':
-        output_raster_format = 'GTiff'
-    else:
-        output_raster_format = output_imagery_format
+        # Run geocode burst workflow (single job)
+        run_single_job(cfg)
 
-    if output_raster_format == 'GTiff':
-        imagery_extension = 'tif'
-    else:
-        imagery_extension = 'bin'
+        _check_results(output_dir_single_job, product_prefix, save_imagery_as_hdf5,
+            save_secondary_layers_as_hdf5, save_metadata, hdf5_file_extension,
+            imagery_extension)
 
-    if save_metadata:
-        geo_h5_filename = f'{output_dir}/'f'{product_prefix}.{hdf5_file_extension}'
 
-    if not save_imagery_as_hdf5:
-        geo_vv_filename = f'{output_dir}/'f'{product_prefix}_VV.{imagery_extension}'
-        # geo_vh_filename = f'{output_dir}/'f'{product_prefix}_VH.{imagery_extension}'
+        # Run geocode burst workflow (parallel)
+        output_dir_parallel = os.path.join('data', 's1b_los_angeles',
+                                        'output_dir_parallel')
 
-    assert(os.path.isfile(geo_h5_filename))
-    assert(os.path.isfile(geo_vv_filename))
-    # assert(os.path.isfile(geo_vh_filename))
+        cfg.groups.product_group.output_dir = output_dir_parallel
+
+        log_file_path = 'log.txt'
+        flag_logger_full_format = False
+        run_parallel(cfg, log_file_path, flag_logger_full_format)
+
+        _check_results(output_dir_parallel, product_prefix, save_imagery_as_hdf5,
+            save_secondary_layers_as_hdf5, save_metadata, hdf5_file_extension,
+            imagery_extension)
+
