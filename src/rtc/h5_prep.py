383beb6231e7c65a1364a8d091026541ebbd9db8
Seongsu Jeong <sjeong.kr@gmail.com>
2023-03-21 01:06:17 -0700

    Update src/rtc/h5_prep.py
    Co-authored-by: Gustavo H. X. Shiroma <52007211+gshiroma@users.noreply.github.com>

--- src/rtc/h5_prep.py
+++ src/rtc/h5_prep.py
@@ -320,9 +320,9 @@ def populate_metadata_group(h5py_obj: h5py.File,
             [np.zeros(2),
              'List indicating the number of lines and samples the RTC-S1 imagery and secondary layers'],
         
-        'identification/productPixelCoordinateConvention': # 1.7.8
-            ['pixel ULC',
-             'Product pixel coordinate convention'],
+        # 'identification/productPixelCoordinateConvention': # 1.7.8
+        #    ['pixel ULC',
+        #     'Product pixel coordinate convention'],
 
 
 
