ccdcfacf51a6ba2623b4d6f340849b143b59b638
Seongsu Jeong <sjeong.kr@gmail.com>
2023-03-30 10:15:04 -0700

    Update src/rtc/h5_prep.py
    Co-authored-by: Gustavo H. X. Shiroma <52007211+gshiroma@users.noreply.github.com>

--- src/rtc/h5_prep.py
+++ src/rtc/h5_prep.py
@@ -355,7 +355,7 @@ def get_metadata_dict(product_id: str,
         'identification/dataAccess': # placeholder for 1.7.1
             ['',
              'URL to access the data'],
-        'identification/productFiltering': # placeholder for 1.7.4
+        'RTC/metadata/processingInformation/parameters/productFiltering': # placeholder for 1.7.4
             [False,
              'Flag if filter has been applied'],
         'identification/productBoundingBox': # placeholder for 1.7.4
