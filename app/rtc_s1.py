b14855cfd9efe83105c83cfa0d35f116a5a0542e
Gustavo H. X. Shiroma <gustavo.h.shiroma@jpl.nasa.gov>
2023-06-16 13:04:16 -0700

    remove unnecessary parameters

--- app/rtc_s1.py
+++ app/rtc_s1.py
@@ -25,7 +25,7 @@ def main():
     create_logger(args.log_file, args.full_log_formatting)
 
     # Get a runconfig dict from command line argumens
-    cfg = RunConfig.load_from_yaml(args.run_config_path, 'rtc_s1')
+    cfg = RunConfig.load_from_yaml(args.run_config_path)
 
     load_parameters(cfg)
 
--- app/rtc_s1_single_job.py
+++ app/rtc_s1_single_job.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     logger = create_logger(args.log_file, args.full_log_formatting)
 
     # Get a runconfig dict from command line argumens
-    cfg = RunConfig.load_from_yaml(args.run_config_path, 'rtc_s1')
+    cfg = RunConfig.load_from_yaml(args.run_config_path)
 
     load_parameters(cfg)
 
