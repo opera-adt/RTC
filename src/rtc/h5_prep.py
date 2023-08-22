3bc1852a93229f8f909ed31d9fddc72ba1c80bd0
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-08-22 14:56:54 -0700

    fix on metadata

--- src/rtc/h5_prep.py
+++ src/rtc/h5_prep.py
@@ -303,7 +303,7 @@ def save_orbit(orbit, orbit_group, orbit_file_path):
     else:
         orbit_type = 'Undefined'
 
-    d = orbit_group.require_dataset("orbitType", (), "S10",
+    d = orbit_group.require_dataset("orbitType", (), "S25",
                                     data=np.string_(orbit_type))
     d.attrs["description"] = np.string_(
         "Type of orbit file used in processing")
@@ -574,8 +574,8 @@ def get_metadata_dict(product_id: str,
         #      'Data processing center'],
         'identification/ceosAnalysisReadyDataDocumentIdentifier':
             ['ceos_analysis_ready_data_document_identifier',
-             'https://ceos.org/ard/files/PFS/NRB/v5.5/CARD4L-PFS_NRB_v5.5.pdf',
              ALL_PRODUCTS,
+             'https://ceos.org/ard/files/PFS/NRB/v5.5/CARD4L-PFS_NRB_v5.5.pdf',
              'CEOS Analysis Ready Data (CARD) document identifier'],
         'identification/dataAccess':
             ['product_data_access',
@@ -795,7 +795,7 @@ def get_metadata_dict(product_id: str,
              '_output_backscatter_expression_convention',
              ALL_PRODUCTS,
              'linear backscatter intensity',
-             'Backscatter expression convension'],
+             'Backscatter expression convention'],
 
         # 3.2
         ('metadata/processingInformation/parameters/'
